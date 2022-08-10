36c9d6940c897e8023417a5c992b58de7a699f57
Ronaldo Lantigua Martines <20198855@itla.edu.do>
2022-08-10 14:22:20 -0400

    edit database connection

--- src/database.js
+++ src/database.js
@@ -3,12 +3,44 @@ const {database} = require('./keys');
 const {promisify} = require('util');
 
 
+  
+  var connection; 
+  
+  function pool() {
+    connection = mysql.createConnection(database); 
+                                                    
+  
+    connection.connect(function(err) {              
+      if(err) {                                     
+        console.log('error when connecting to db:', err);
+        setTimeout(pool, 2000); 
+      }                                    
+    });                                     
+                                            
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        pool();                         
+      } else {                                      
+        throw err;                                  
+      }
+    });
+  }
+  pool()
+  module.exports = pool;
+
+
+
+/*
 const pool = mysql.createPool(database);
 
 pool.getConnection((err, connection)=>{
+    
     if(err){
         if(err === 'PROTOCOL_CONNECTION_LOST'){
+            
             console.error('La coneccion con la BD fue cerrada')
+
         }
         if(err === 'ER_CON_COUNT_ERROR'){
             console.error('La DB tiene muchas conneciones')
@@ -27,5 +59,4 @@ pool.getConnection((err, connection)=>{
 });
 
 pool.query = promisify(pool.query);
-
-module.exports = pool;
+*/
--- src/keys.js
+++ src/keys.js
@@ -1,16 +1,13 @@
 module.exports = {
     database: {
-        connectionLimit : 1000,
-        connectTimeout  : 60 * 60 * 1000,
-        acquireTimeout  : 60 * 60 * 1000,
-        timeout         : 60 * 60 * 1000, 
+
         host: 'ec2-54-159-175-38.compute-1.amazonaws.com',
         port: 5432,
         database: 'd68ks9s8mdss0c',
         user: 'snslkibzurltfy',
         password: '70d41e371bea89569fc6eb56d6d1f1511ad73fd88e4c9095e9c360831538da77',
-        URI: 'postgres://snslkibzurltfy:70d41e371bea89569fc6eb56d6d1f1511ad73fd88e4c9095e9c360831538da77@ec2-54-159-175-38.compute-1.amazonaws.com:5432/d68ks9s8mdss0c',
         
+
     }
 }
 
